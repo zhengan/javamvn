2856081dddb2cb0479b73f8142757ac548ced37b
zengding <547796984@qq.com>
2023-01-05 20:33:31 +0800

    my first code

---
+++ java-foundational/src/main/java/net/dza/java/HelloWork.java
@@ -0,0 +1,14 @@
+package net.dza.java;
+
+/**
+ * my first
+ *
+ * @author dza 547796984@qq.com
+ * @version 2023/1/5 8:29 PM
+ * @since JDK17
+ */
+public class HelloWork {
+    public static void main(String[] args) {
+        System.out.println("Hello world with mac git mvn idea");
+    }
+}
